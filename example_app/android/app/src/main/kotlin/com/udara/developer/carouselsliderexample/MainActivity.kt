895adc144b004ab6d5f3bc1e4ed97af20c1f0c17
UdaraWanasinghe <udara.developer@gmail.com>
2021-03-14 07:51:03 +0530

    code format

--- example_app/android/app/src/main/kotlin/com/udara/developer/carouselsliderexample/MainActivity.kt
+++ example_app/android/app/src/main/kotlin/com/udara/developer/carouselsliderexample/MainActivity.kt
@@ -2,5 +2,5 @@ package com.udara.developer.carouselsliderexample
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
 }
--- example_app/android/app/src/main/kotlin/com/udara/developer/example_app/MainActivity.kt
+++ example_app/android/app/src/main/kotlin/com/udara/developer/example_app/MainActivity.kt
@@ -2,5 +2,5 @@ package com.udara.developer.example_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
 }
